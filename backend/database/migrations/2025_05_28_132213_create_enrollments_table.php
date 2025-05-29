a9a9fb5bd7ae4a5cc11d7f291c874b8724b77c23
nguyenhuyit2308 <huyreunglang123@gmail.com>
2025-05-29 14:39:55 +0700

    add database into migration

---
+++ backend/database/migrations/2025_05_28_132213_create_enrollments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enrollments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
+            $table->timestamp('enrolled_at')->useCurrent();
+            $table->enum('status', ['enrolled', 'completed', 'dropped'])->default('enrolled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enrollments');
+    }
+};
